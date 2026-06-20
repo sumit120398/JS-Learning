35713cea4bd58701785f9eb08185fa9eca4c9ca1
Sumit Channawar <12sumitchannawar59@gmail.com>
2026-06-20 18:52:53 +0530

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,26 @@
+"use strict"; //treat all JS code as newer version
+
+// alert("hello") // we are using nodejs, not browser
+
+console.log(3 + 3)
+
+console.log("Sumit")
+
+let name = "Sumit"
+let age = 25
+let isLoggedIn = false
+let state;
+
+// number => 2 to power 53
+// biging
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined =>
+// symbol => unique
+
+
+// object
+
+console.log(typeof null); // object
+console.log(typeof undefined); // undefined
