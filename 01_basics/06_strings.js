bc522ccaca1566e1e8c6de76d825e98ba0e1704b
Sumit Channawar <sumit.channawar@fortra.com>
2026-07-18 23:22:22 +0530

    String methods

---
+++ 01_basics/06_strings.js
@@ -0,0 +1,34 @@
+const name = "Sumit"
+const repoCount = 50
+
+// console.log(name + repoCount + " Value");
+
+console.log(`Hello my name is ${name} and my repo count is ${repoCount}`)
+
+const gameName = new String('Sumit-sc-com')
+
+// console.log(gameName[0]);
+// console.log(gameName.__proto__);
+
+
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+// console.log(gameName.charAt(2));
+console.log(gameName.indexOf('t'));
+
+const newString = gameName.substring(0, 4)
+console.log(newString);
+
+const anotherString = gameName.slice(-8, 4)
+console.log(anotherString);
+
+const newStringOne = "  sumit  "
+console.log(newStringOne);
+console.log(newStringOne.trim());
+
+const url = "https://sumit.com/sumit%20channawar"
+console.log(url.replace('%20', '-'))
+
+console.log(url.includes('sundar'))
+
+console.log(gameName.split('-'))
